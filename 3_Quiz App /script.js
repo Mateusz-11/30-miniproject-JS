6894113b86e2d9693598ff5ef1d32cba21de73b9
Mateusz-11 <tweak.night@gmail.com>
2024-02-20 21:55:07 +0100

    Added data with questions

---
+++ 3_Quiz App /script.js
@@ -0,0 +1,38 @@
+const questions = [
+	{
+		question: "Whish is the largest animal in the world?",
+		answer: [
+			{ text: "Shark", correct: false },
+			{ text: "Blue whale", correct: true },
+			{ text: "Elephant", correct: false },
+			{ text: "Giraffe", correct: false },
+		],
+	},
+	{
+		question: "Whish is the smallest country in the world?",
+		answer: [
+			{ text: "Vatican City", correct: true },
+			{ text: "Bhutan", correct: false },
+			{ text: "Nepal", correct: false },
+			{ text: "Shri Lanka", correct: false },
+		],
+	},
+	{
+		question: "Whish is the largest desert in the world?",
+		answer: [
+			{ text: "Kalahari", correct: false },
+			{ text: "Gobi", correct: false },
+			{ text: "Sahara", correct: false },
+			{ text: "Antarctica", correct: true },
+		],
+	},
+	{
+		question: "Whish is the smallest continent in the world?",
+		answer: [
+			{ text: "Asia", correct: false },
+			{ text: "Australia", correct: true },
+			{ text: "Arctica", correct: false },
+			{ text: "Africa", correct: false },
+		],
+	},
+];
