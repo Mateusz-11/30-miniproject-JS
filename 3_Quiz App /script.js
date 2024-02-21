87432379a03e3e2576b5165fbe979145e12867cf
Mateusz-11 <tweak.night@gmail.com>
2024-02-21 17:55:30 +0100

    Next functions

--- 3_Quiz App /script.js
+++ 3_Quiz App /script.js
@@ -52,6 +52,7 @@ function startQuiz() {
 }
 
 function showQuestion() {
+	resetState();
 	let currentQuestion = questions[currentQuestionIndex];
 	let questionNo = currentQuestionIndex + 1;
 	questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -60,7 +61,34 @@ function showQuestion() {
 		button.innerHTML = answer.text;
 		button.classList.add("btn");
 		answerButton.appendChild(button);
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
+		}
+		button.addEventListener("click", selectAnswer);
 	});
+}
+function resetState() {
+	nextButton.style.display = "none";
+	while (answerButtons.firstChild) {
+		answerButtons.removeChild(answerButtons.firstChild);
+	}
+}
+
+function selectAnswer(e) {
+	const selectBtn = e.target;
+	const isCorrect = selectBtn.dataset.correct === "correct";
+	if (isCorrect) {
+		selectBtn.classList.add("correct");
+	} else {
+		selectBtn.classList.add("incorrect");
+	}
+	Array.from(answerButtons.chldren).forEach((button) => {
+		if (button.dataset.correct === "true") {
+			button.classList.add("correct");
+		}
+		button.disabled = true;
+	});
+	nextButton.style.display = "block";
 }
 
 startQuiz();
